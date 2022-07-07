baf676303d670ef0180c5159efcdcb96ae82f971
mundoenrique <mundoenrique@hotmail.com>
2022-07-06 20:46:22 -0400

    Nuenas funciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from './store/slices/counter';
 import logo from './logo.svg';
 import './App.css';
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const { counter } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p>count is: {counter}</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
+          <button type="button" onClick={() => dispatch(increment())}>
+            Increment
+          </button>
+          <button type="button" onClick={() => dispatch(decrement())}>
+            Decrement
+          </button>
+          <button type="button" onClick={() => dispatch(incrementByAmount(2))}>
+            Increment By 2
           </button>
         </p>
       </header>
